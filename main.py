69f7a6efdc8ac7432b532bf6594590e812febcfa
Drew Dru <drew.drux@gmail.com>
2018-06-22 22:47:33 +0700

    get args

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ import krb5_conf
 import host_conf
 import resolv_conf
 import ntp_conf
+import config
+import main_gui
 
 def main(app_conf):
     resolv_conf.update_resolv_conf(app_conf)
-    if os.system('sudo /etc/init.d/network-manager restart') != 0:
-        os.system('sudo /etc/init.d/networking restart')
     host_conf.set_hostname(app_conf)
     host_conf.set_hosts(app_conf)
     ntp_conf.time_autosinc(app_conf)
     os.system('sudo /etc/init.d/ntp restart')
     krb5_conf.change_krb5_conf(app_conf)
 
+class StoreDictKeyPair(argparse.Action):
+     def __call__(self, parser, namespace, values, option_string=None):
+         my_arr = []
+         for kd in values.split(","):
+            my_dict = {}
+            for kv in kd.split("+"):
+                k,v = kv.split("=")
+                my_dict[k] = v
+            my_arr.append(my_dict)
+         setattr(namespace, self.dest, my_arr)
+
 if __name__ == '__main__':
-    # test = os.popen('whoami | id | grep sudo -c').read()
-    # print(type(test))
     dependencies.update_packages()
     if len(sys.argv) == 1:
         app_conf = talk_with_user.get_params()
@@ -35,13 +44,13 @@ if __name__ == '__main__':
             action='store_true',
             help='Запустить с графическим интерфейсом')
         arg_parser.add_argument('-d', '--domain',
-            nargs=1,
+            nargs='?',
             help='Домен для входа\tПример: domain.com')
         arg_parser.add_argument('--hostname',
-            nargs=1,
+            nargs='?',
             help='HOSTNAME данного компьютера\tПример: smbsrv01')
         arg_parser.add_argument('-t', '--time_server',
-            nargs=1,
+            nargs='?',
             help='Сервер для автоматической синхронизации\tПример: dc.domain.com')
         arg_parser.add_argument('--auto_resolv',
             action='store_true',
@@ -49,14 +58,28 @@ if __name__ == '__main__':
         arg_parser.add_argument('--dhcp',
             action='store_true',
             help='IP-адрес динамический и присваивается DHCP сервером?')
+        arg_parser.add_argument("--dns_list", 
+            action=StoreDictKeyPair, 
+            help="Список DNS домена (host=dc+ip=192.168.0.1+is_admin_server=True,host=dc2+ip=192.168.0.2)")
         args = arg_parser.parse_args()
-        print(args)
+        
+        if args.domain is not None:
+            config.app_conf['DOMAIN'] = args.domain
+        if args.hostname is not None:
+            config.app_conf['HOSTNAME'] = args.hostname
+        if args.time_server is not None:
+            config.app_conf['TIME_SERVER'] = args.time_server
+        if args.dns_list is not None:
+            config.app_conf['DNS_LIST'] = args.dns_list
+        config.app_conf['IS_AUTO_RESOLV'] = args.auto_resolv
+        config.app_conf['IS_DHCP'] = args.dhcp
+        print(config.app_conf)
+        exit(0)
         if args.gui:
-            # main_gui()
+            main_gui.main()
             print('run with GUI')
         else: 
-            print('no GUI')
-        # sys.exit(main(args.date, args.rar))
+            sys.exit(main(config.app_conf))
 
 
 
