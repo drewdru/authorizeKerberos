503223cfebff5f43d25f85daa01710a83c2b5376
Drew Dru <drew.drux@gmail.com>
2018-06-22 22:45:37 +0700

    bug fixed

--- dependencies.py
+++ dependencies.py
@@ -1,4 +1,5 @@
 #! ./env/bin/python
+# -*- coding: utf-8 -*-
 import os
 import sys
 import file_helper
@@ -6,19 +7,27 @@ from packages_conf import APT_PACKAGES
 
 def install_sudo():
     user = os.popen('whoami').read()
+    os.system('su -c "apt-get update"')
+    os.system('su -c "apt-get -y upgrade"')
+    os.system('su -c "apt-get -y dist-upgrade"')
     os.system('su -c "apt-get -y install sudo"')
-    os.system('usermod -aG sudo {}'.format(user))
-    file_helper.add_string_to_file('{} ALL=(ALL) ALL'.format(user), '/etc/sudoers')
-    file_helper.replace_file_line('/etc/sudoers', old_line, prepend_dns)
+    os.system('su -c "usermod -aG sudo {}"'.format(user))
+    file_helper.add_string_to_file('{} ALL=(ALL) ALL'.format(user.rstrip()), '/etc/sudoers')
 
 def install_apt_packages():
     os.system('sudo apt-get update')
     os.system('sudo apt-get -y upgrade')
     os.system('sudo apt-get -y dist-upgrade')
+    if 'apt-utils' in APT_PACKAGES:
+        os.system('sudo apt-get install -y --no-install-recommends apt-utils')
     if 'krb5-user' in APT_PACKAGES:
-        os.system('\n | sudo apt-get -y install krb5-user')
-    os.system('sudo apt-get -y install {}'.format(' '.join(APT_PACKAGES)))
-    
+        print('KRB5!!!!!!!!!!!!!!!!!!!!!')
+        # os.system('export DEBIAN_FRONTEND=teletype')
+        # os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
+        os.system('yes "\n" | sudo apt-get -y --assume-yes install krb5-user')
+    print('sudo apt-get -y install {}'.format(' '.join(APT_PACKAGES)))
+    os.system('sudo apt-get -y --assume-yes install {}'.format(' '.join(APT_PACKAGES)))
+
 if os.system('sudo apt-get -v') != 0:
     install_sudo()
 
@@ -26,26 +35,30 @@ def update_packages():
     for apt_package in APT_PACKAGES:
         is_install = os.system('dpkg -l {} | grep "ii  {}"'.format(apt_package, apt_package))
         if is_install != 0:
-            if apt_package == 'sudo':
+            if os.system('sudo apt-get -v') != 0:
                 install_sudo()
             install_apt_packages()
             break
-    
-    with open('/etc/default/locale', "r+") as file:
-        for line in file:
-            if 'LANG=en_US.UTF-8' in line:
-                break
-            if 'LANGUAGE=en_US.UTF-8' in line:
-                break
-        else:
-            os.system('locale-gen --purge en_US.UTF-8')
-            file.write('LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8')
+    try:
+        with open('/etc/default/locale', "r+") as file:
+            for line in file:
+                if 'LANG=en_US.UTF-8' in line:
+                    break
+                if 'LANGUAGE=en_US.UTF-8' in line:
+                    break
+            else:
+                # os.system('sudo echo "en_US.UTF-8 UTF-8" > /etc/locale.gen')
+                os.system('sudo locale-gen --purge en_US.UTF-8')
+                file.write('LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8')
+    except FileNotFoundError:
+        os.system('sudo locale-gen --purge en_US.UTF-8')
+        os.system('sudo echo -e "LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" > /etc/default/locale')
 
-    with open('/etc/default/locale', "r+") as file:
-        for line in file:
-            if 'allow-hotplug eth0' in line:
-                break
-            if 'iface eth0 inet dhcp' in line:
-                break
-        else:
-            file.write('\nallow-hotplug eth0\niface eth0 inet dhcp')
+    os.system('sudo echo "en_US UTF-8" > /etc/locale.gen')
+    os.environ['LC_ALL'] = 'en_US.UTF-8'
+    os.environ['LANG'] = 'en_US.UTF-8'
+    os.environ['LANGUAGE'] = 'en_US.UTF-8'
+    os.system('sudo locale-gen en_US.UTF-8')
+    os.system('sudo update-locale LANG=en_US')
+
+
